9fd7e055de341ca47bdcad1daf80a3e33974d2d9
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-08-03 16:26:12 +0500

    cors do bar laga dia tha galti se

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const cors = require("cors")
 const connectDB = require("./config/db.js")
 const userRoutes = require("./routes/user.routes.js")
 const cors = require("cors")
